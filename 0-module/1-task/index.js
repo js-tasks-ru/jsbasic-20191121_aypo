dc90f8b334a84ec9ee08ce3fa22ed415ef9c3fdd
Ay Po <pomesha@gmail.com>
2019-11-19 13:08:36 +0500

    суммирование через return

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,12 +1,3 @@
-/**
- * Power
- * @param {number} m base
- * @param {number} n index
- * @returns {number}
- */
-
 function sum(m, n) {
-  return m + n;
+	return m + n
 }
-sum(1, 1) === 2;
-sum(2, 5) === 7;
