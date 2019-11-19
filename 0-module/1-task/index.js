0d09ad0cf587573e634562d6b85838ed405d9633
Ay Po <pomesha@gmail.com>
2019-11-19 12:36:18 +0500

    суммирование через return

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -4,31 +4,9 @@
  * @param {number} n index
  * @returns {number}
  */
- /*
-function sum(m, n) {
-	let a = +prompt("Введите первое число m", " ");
-	let b = +prompt("Введите второе число n", " ");
-	let x = a + b;
-	let phrase;
-
-	if (x === 2) {
-		phrase = "Ура, вы получили " + x;
-	} else if (x === 7) {
-		phrase = "Ура, вы получили " + x;
-	} else {
-		phrase = "Ошибка("
-	}
-
-	alert(phrase);
-}
-
-sum()
-*/
 
 function sum(m, n) {
   return m + n;
 }
-let result = sum(1, 1) === 2;
-let result2 = sum(2, 5) === 7;
-alert (result);
-alert (result2);
+sum(1, 1) === 2;
+sum(2, 5) === 7;
