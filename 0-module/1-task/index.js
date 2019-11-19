d08f368c8220162b9fa652b12299a234fc4154fc
Ay Po <pomesha@gmail.com>
2019-11-19 12:10:10 +0500

    через return и alert

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -4,6 +4,31 @@
  * @param {number} n index
  * @returns {number}
  */
+ /*
 function sum(m, n) {
+	let a = +prompt("Введите первое число m", " ");
+	let b = +prompt("Введите второе число n", " ");
+	let x = a + b;
+	let phrase;
 
+	if (x === 2) {
+		phrase = "Ура, вы получили " + x;
+	} else if (x === 7) {
+		phrase = "Ура, вы получили " + x;
+	} else {
+		phrase = "Ошибка("
+	}
+
+	alert(phrase);
 }
+
+sum()
+*/
+
+function sum(m, n) {
+  return m + n;
+}
+let result = sum(1, 1) === 2;
+let result2 = sum(2, 5) === 7;
+alert (result);
+alert (result2);
