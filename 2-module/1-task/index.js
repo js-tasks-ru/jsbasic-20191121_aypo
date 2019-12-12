5552599840aca262d66a09d9a6e4b33107be4755
javascript-ru <47786167+jsru@users.noreply.github.com>
2019-12-12 22:39:15 +0300

    Added solution for task: Функция clone(obj)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,19 @@
  * @returns {Object}
  */
 function clone(obj) {
+  const result = {};
+
+  if (typeof obj !== 'object') {
+    return obj;
+  }
+
+  for (const key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      result[key] = clone(obj[key]);
+    } else {
+      result[key] = obj[key];
+    }
+  }
+
+  return result;
 }
