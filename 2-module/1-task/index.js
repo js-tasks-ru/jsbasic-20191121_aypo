adc491b4a478ee17e9d47f9674bebb0edc68e3e1
Ay Po <pomesha@gmail.com>
2019-12-13 14:55:27 +0500

    подсмотрел клонирование

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -5,11 +5,19 @@
  */
 
 function clone(obj) {
+  const result = {};
 
-  for (let key in obj) {
-    if (!obj) {
-    return;
-    }
-     
+  if (typeof obj !== 'object') {
+    return obj;
   }
+
+  for (const key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      result[key] = clone(obj[key]);
+    } else {
+      result[key] = obj[key];
+    }
+  }
+
+  return result;
 }
